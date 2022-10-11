5939dac140c259646c49f3ec19fde93a86a205d8
Elena Bruk <aen_law@mail.ru>
2022-10-12 00:46:53 +0300

    Task 43 bag fixed

--- SecondApp/Program.cs
+++ SecondApp/Program.cs
@@ -1,4 +1,7 @@
-﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
 double GetValue(string text)
 {
     Console.Write($"Введите значение коэффициента {text}: ");
@@ -6,13 +9,12 @@ double GetValue(string text)
     return (value);
 }
 
-double k1 = GetValue("k1");
-double b1 = GetValue("b1");
-double k2 = GetValue("k2");
-double b2 = GetValue("b2");
-
 void GetCrossPoint()
 {
+    double k1 = GetValue("k1");
+    double b1 = GetValue("b1");
+    double k2 = GetValue("k2");
+    double b2 = GetValue("b2");
     double x = Math.Round((b2 - b1) / (k1 - k2), 2);
     double y = Math.Round((k1 * x + b1), 2);
     if (k1 == k2)
